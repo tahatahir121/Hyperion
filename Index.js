8e618e64496afd3cc4ab8e47c995eae71d0dfbae
Taha Tahir <Tahatahir566@gmail.com>
2026-09-12 20:43:25 +0500

    comit

--- Index.js
+++ Index.js
@@ -178,9 +178,3 @@
     });
   });
 })();
-
-    fetch("navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        });
